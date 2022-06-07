ae4c9bbb69c598c05d7c5d7e8aa17f04c09ff287
Nguyen Hong Doanh <doanh2001bn@gmail.com>
2022-06-07 19:19:49 +0700

    LAB03_1

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { render } from "@testing-library/react";
 import { Component } from "react";
+import Menu from "./components/Menucomponent";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
