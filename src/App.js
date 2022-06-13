e7e6e1fde30329c260dbb70cf081934fb89d8caa
Nguyen Hong Doanh <doanh2001bn@gmail.com>
2022-06-13 20:50:04 +0700

    LAB05_2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
-import { Navbar, NavbarBrand } from "reactstrap";
-import { render } from "@testing-library/react";
-import { Component } from "react";
-import Menu from "./components/Menucomponent";
-import { DISHES } from "./shared/dish";
+import React, { Component } from "react";
 import Main from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
