d54d91a190e0263f841843e9b885e521be5a8fbc
Nguyen Hong Doanh <doanh2001bn@gmail.com>
2022-06-19 15:50:22 +0700

    LAB06_1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Contact from "./ContactComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import About from "./AbotUsComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -36,7 +37,6 @@ class Main extends Component {
     };
 
     const DishWithId = ({ match }) => {
-      console.log(parseInt(match.params.dishId, 10));
       return (
         <DishDetail
           dish={
@@ -63,6 +63,11 @@ class Main extends Component {
           />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to="/home" />
         </Switch>
         <Footer />
